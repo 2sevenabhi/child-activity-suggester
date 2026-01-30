dc07d7385c4e49a00e5327e4579282c9c52f0529
Abhishek Mishra <agpml5299@gmail.com>
2026-01-30 23:55:20 +0530

    initial spring boot project setup

---
+++ src/main/java/com/parentapp/activity/ActivitySuggesterApplication.java
@@ -0,0 +1,13 @@
+package com.parentapp.activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitySuggesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitySuggesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parentapp/activity/ActivitySuggesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parentapp.activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitySuggesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
