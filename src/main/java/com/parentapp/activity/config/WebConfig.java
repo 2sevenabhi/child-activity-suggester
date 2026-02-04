327dc62cab5878dcb1fd324a88743273577d676a
Abhishek Mishra <agpml5299@gmail.com>
2026-02-04 09:18:55 +0530

    Added config file to deal with CORS error

---
+++ src/main/java/com/parentapp/activity/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.parentapp.activity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:8081",
+                        "http://localhost:19006",
+                        "https://child-activity-suggester.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
